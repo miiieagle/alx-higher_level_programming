518e66a1b5a16df16b385cf6bf2495114004e794
miieagle <miieagle06@gmail.com>
2022-12-07 11:36:45 -0800

    I made a 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-def multiply_by_2(a_dictionary):
-    new_dir = a_dictionary.copy()
-    list_keys = list(new_dir.keys())
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return (None)
 
-    for i in list_keys:
-        new_dir[i] *= 2
-
-    return (new_dir)
+    return (max(a_dictionary, key=a_dictionary.get))
