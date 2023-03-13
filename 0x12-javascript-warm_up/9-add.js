8e7a935aa1aa938f0ad425166e4dac44b7304067
miieagle <miieagle06@gmail.com>
2023-03-13 20:20:09 +0000

    Files containing solution to the main tasks

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,16 @@
+t x = process.argv[2];
+
+if (!parseInt(x)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < x; i++) {
+    let y = 0;
+    let myVar = '';
+
+    while (y < x) {
+      myVar = myVar + 'X';
+      y++;
+    }
+    console.log(myVar);
+  }
+}
